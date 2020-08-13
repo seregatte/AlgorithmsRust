f31efee221d4763ede272dd717faf87f71c974f0
João Paulo Seregatte Costa <joao.costa@itelios.com>
2020-08-13 18:46:03 -0300

    Reverse String - Solution 3

--- src/algorithms/reversestring.rs
+++ src/algorithms/reversestring.rs
@@ -1,11 +1,20 @@
+// Solution 1
 // pub fn reverse(s: &str) -> String {
 //     s.chars().rev().collect::<String>()
 // }
+// Solution 2
+// pub fn reverse(s: &str) -> String {
+//     s.chars().fold("".to_string(), |mut reversed, c| {
+//         reversed.insert(0 , c);
+//         reversed
+//     })
+// }
+
+
 
 pub fn reverse(s: &str) -> String {
-    s.chars().fold("".to_string(), |mut reversed, c| {
-        reversed.insert(0 , c);
-        reversed
+    s.chars().fold("".to_string(), |reversed, c| {
+        format!("{}{}", c, reversed)
     })
 }
 
